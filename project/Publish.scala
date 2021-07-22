68e134a77aa98852f75cb8e102162511b7603640
Arnout Engelen <arnout@bzzt.net>
2021-07-22 12:50:46 +0200

    Add metadata required for sonatype releases

--- project/Publish.scala
+++ project/Publish.scala
@@ -7,6 +7,13 @@ object Publish extends AutoPlugin {
   override def trigger = allRequirements
 
   override def projectSettings = Seq(
+    homepage := Some(url("https://developer.lightbend.com/docs/paradox")),
+    developers := List(
+      Developer(
+        "akka-contributors",
+        "Akka Contributors",
+        "akka.official@gmail.com",
+        url("https://github.com/akka/akka-paradox/graphs/contributors"))),
     licenses += "Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html"),
   )
 
