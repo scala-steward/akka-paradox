e1d0062a8d64c761509ec3b19137502e9c106b7b
Arnout Engelen <arnout@bzzt.net>
2021-07-22 12:44:11 +0200

    Set sonatypeProfileName on project root

--- project/Publish.scala
+++ project/Publish.scala
@@ -1,15 +1,10 @@
 import sbt._
 import sbt.Keys._
 import sbt.plugins.JvmPlugin
-import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
 
 object Publish extends AutoPlugin {
 
   override def trigger = allRequirements
-
-  override def buildSettings = Seq(
-    sonatypeProfileName := "com.lightbend",
-  )
 
   override def projectSettings = Seq(
     licenses += "Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html"),
